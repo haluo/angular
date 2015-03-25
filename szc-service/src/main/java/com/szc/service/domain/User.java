b199091c31438ed3721a2301dd2046046d7526e3
layman <zhufeng3612@163.com>
2015-03-25 10:58:40 +0800

    user

--- szc-service/src/main/java/com/szc/service/domain/User.java
+++ szc-service/src/main/java/com/szc/service/domain/User.java
@@ -1,28 +1,61 @@
 package com.szc.service.domain;
 
+import java.util.Date;
+
 /**
  * Created by shizhizhong on 15/2/10.
  */
 public class User extends BaseDomain{
 
-    private Integer id;
-    private String wxId;
+    private Long id;
+    private String uname;
+    private String passwd;
+    private String ename;
+    private String email;
     private String phone;
+    private Integer provinceid;
+    private Integer cityid;
+    private Date created;
+    private Date modified;
 
-    public Integer getId() {
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getWxId() {
-        return wxId;
+    public String getUname() {
+        return uname;
     }
 
-    public void setWxId(String wxId) {
-        this.wxId = wxId;
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPhone() {
@@ -31,5 +64,37 @@ public class User extends BaseDomain{
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public Integer getProvinceid() {
+        return provinceid;
+    }
+
+    public void setProvinceid(Integer provinceid) {
+        this.provinceid = provinceid;
+    }
+
+    public Integer getCityid() {
+        return cityid;
+    }
+
+    public void setCityid(Integer cityid) {
+        this.cityid = cityid;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
     }
 }
